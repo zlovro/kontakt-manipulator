d4c0d55a2e4865b73c015f3b74810fa45d138556
zlovro <zedniklovro@gmail.com>
2025-11-24 19:09:55 +0100

    Initial commit

---
+++ FormPopupProgress.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace smanip
+{
+    public partial class FormPopupProgress : Form
+    {
+        public FormPopupProgress()
+        {
+            InitializeComponent();
+        }
+    }
+}
